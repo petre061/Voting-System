97e29e2e09bf238ad8c0c1c70fd02f7210b6e2c0
Alex Bohm <bohm0080@umn.edu>
2021-03-10 22:52:33 -0600

    Added AuditLog and MediaReport filename date/time, more testing for IRBallot

--- Project1/src/Election.h
+++ Project1/src/Election.h
@@ -1,7 +1,7 @@
 // Copyright CSCI 5801 Spring 2021 Team 20
 
-#ifndef ELECTION_H_
-#define ELECTION_H_
+#ifndef PROJECT1_SRC_ELECTION_H_
+#define PROJECT1_SRC_ELECTION_H_
 
 #include <fstream>
 #include <string>
@@ -14,22 +14,50 @@
  * @brief
  */
 class Election : public Loggable {
+ private:
+  /**
+   * @brief Make a string with the date seperated by underscores
+   */
+  static std::string make_file_date();
+
  protected:
+  /**
+   * @brief The type of election.
+   */
   std::string type = "UNKNOWN ELECTION TYPE";
 
+  /**
+   * @brief The name of the file that holds the ballots.
+   */
   std::string ballot_filename;
+  /**
+   * @brief The actual file handler for the ballot file.
+   */
   std::ifstream ballot_file;
-
+  /**
+   * @brief The @ref AuditLog instance for an election.
+   */
   AuditLog audit_log;
+  /**
+   * @brief The @ref MediaReport instance for an election.
+   */
   MediaReport media_report;
-
+  /**
+   * @brief The total number of ballots contained in the ballots file.
+   */
   uint64_t total_ballots = 0;
 
  public:
+  /**
+   * @brief Start an election setting the type and opening the ballots file
+   *
+   * This must be called in the initializer list of the inheriting type's
+   * constructor.
+   */
   explicit Election(const std::string& election_type,
                     const std::string& ballots);
   virtual ~Election();
   virtual int run() = 0;
 };
 
-#endif
+#endif  // PROJECT1_SRC_ELECTION_H_
--- Project1/testing/test_IRBallot.cc
+++ Project1/testing/test_IRBallot.cc
@@ -1,6 +1,8 @@
 // Copyright 2021, CSCI 5801 Spring 2021 Team 20
 #include "test_IRBallot.h"
 
+#include <string>
+
 #include "IRBallot.h"
 
 TEST_F(fixture_IRBallot, IRBallotCSVParse) {
@@ -35,4 +37,41 @@ TEST_F(fixture_IRBallot, IRBallotIncrement) {
   // Choice should now be max (out of range of candidate)
   ballot->increment_choice();
   ASSERT_EQ(ballot->get_choice(), Ballot::NO_CHOICE);
+}
+
+TEST_F(fixture_IRBallot, IRBallotLog) {
+  ballot = new IRBallot("1,2,3,4");
+
+  std::string id = std::to_string(ballot->get_id());
+  std::string expected;
+
+  expected = "IRBallot: id=" + id +
+             " choices=[0, 1, 2, 3] choice_index=0 get_choice()=0";
+  ASSERT_EQ(ballot->log(), expected);
+
+  ballot->increment_choice();
+
+  expected = "IRBallot: id=" + id +
+             " choices=[0, 1, 2, 3] choice_index=1 get_choice()=1";
+  ASSERT_EQ(ballot->log(), expected);
+
+  ballot->increment_choice();
+
+  expected = "IRBallot: id=" + id +
+             " choices=[0, 1, 2, 3] choice_index=2 get_choice()=2";
+  ASSERT_EQ(ballot->log(), expected);
+
+  ballot->increment_choice();
+
+  expected = "IRBallot: id=" + id +
+             " choices=[0, 1, 2, 3] choice_index=3 get_choice()=3";
+  ASSERT_EQ(ballot->log(), expected);
+
+  ballot->increment_choice();
+
+  std::string max_choice = std::to_string(Ballot::NO_CHOICE);
+
+  expected = "IRBallot: id=" + id +
+             " choices=[0, 1, 2, 3] choice_index=4 get_choice()=" + max_choice;
+  ASSERT_EQ(ballot->log(), expected);
 }
