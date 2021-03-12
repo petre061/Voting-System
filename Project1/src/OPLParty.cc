56396ca07e2272dabe553ed27e7a9b030deb91bf
Nikhil Srikanth <srika018@umn.edu>
2021-03-12 13:38:06 -0600

    Finished base logic for OPL elections

--- Project1/src/OPLParty.cc
+++ Project1/src/OPLParty.cc
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <map>
+#include <algorithm>
 
 OPLParty::OPLParty(const std::string& name) {
   pname = name;
@@ -34,10 +36,31 @@ std::string OPLParty::log() const {
     }   
     output << "]";
     return output.str();
+  }
 }
 
 std::string OPLParty::get_name(){
   return pname;
 }
+
+std::vector<std::string> OPLParty::get_top_n_candidate_names(int n) {
+  std::map<int, std::string> names;
+  std::vector<int> totals;
+  std::vector<std::string> result;
+  for(int i = 0; i < candidates.size(); i++) {
+    names[candidates.at(i)->get_tally()] = candidates.at(i)->get_name();
+    totals.push_back(candidates.at(i)->get_tally());
+  }
+  for(int i = 0; i < n; i++) {
+    auto highest_tally = std::max_element(std::begin(totals), std::end(totals));
+    int index = std::distance(std::begin(totals), highest_tally);
+    result.push_back(names[*highest_tally]);
+    totals.at(index) = 0;
+  }
+
+  return result; 
+}
+
+
 
 // Creating / adding to candidates DS done in OPLElection class?
