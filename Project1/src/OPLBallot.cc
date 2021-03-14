3f3d451e8abee7d721066fd02215afd46527979d
Alex Bohm <bohm0080@umn.edu>
2021-03-13 19:09:47 -0600

    Added TieBreaker test and fixed compile issue

--- Project1/src/OPLBallot.cc
+++ Project1/src/OPLBallot.cc
@@ -1,9 +1,10 @@
 // Copyright CSCI 5801 Spring 2021 Team 20
 #include "OPLBallot.h"
+
 #include <iostream>
-#include <vector>
 #include <sstream>
 #include <string>
+#include <vector>
 // Step 1: Create stream from given string
 // Step 2: While the stream is not completed
 // Step 2.1: Take item before comma
@@ -12,25 +13,26 @@
 
 OPLBallot::OPLBallot(const std::string& line) {
   // TODO(Alex, Peter): Implement line parsing
-  if(line.length() == 0) {
+  if (line.length() == 0) {
     throw std::invalid_argument("Invalid Ballot - Empty");
   }
   std::string line2parse = line;
-    std::vector<std::string> parsedline;
-    std::istringstream s_stream(line2parse);
-    uint8_t i = 0;
-    while(s_stream.good()) {
-        std::string substr;
-        getline(s_stream, substr, ',');
-        if(substr == "1") {
-            choice = i;
-            exit(0);
-        }
-        i++;
-        parsedline.push_back(substr);
+  std::vector<std::string> parsedline;
+  std::istringstream s_stream(line2parse);
+  uint8_t i = 0;
+  while (s_stream.good()) {
+    std::string substr;
+    getline(s_stream, substr, ',');
+    if (substr == "1") {
+      choice = i;
+      // TODO: verify return is the correct statement here
+      // exit(0);
+      return;
     }
-    throw std::invalid_argument("Invalid Ballot - no choice selected");
-
+    i++;
+    parsedline.push_back(substr);
+  }
+  throw std::invalid_argument("Invalid Ballot - no choice selected");
 }
 uint8_t OPLBallot::get_choice() const {
   // Return the current choice
@@ -42,7 +44,8 @@ std::string OPLBallot::log() const {
   std::stringstream output;
 
   // Put the current choice into the stream
-  output << "OPLBallot " << std::to_string(get_id()) << " get_choice()=" << std::to_string(get_choice());
+  output << "OPLBallot " << std::to_string(get_id())
+         << " get_choice()=" << std::to_string(get_choice());
 
   // Return the stream converted to a string
   return output.str();
--- Project1/testing/test_TieBreaker.cc
+++ Project1/testing/test_TieBreaker.cc
@@ -2,7 +2,10 @@
 
 #include "test_TieBreaker.h"
 
-TEST_F(fixture_TieBreaker, unimplemented) {
-  // TODO(someone): Write this test
-  FAIL() << "Implement this test";
+TEST_F(fixture_TieBreaker, TieBreakerInRange) {
+  uint8_t winner = 0;
+  for (uint64_t i = 0; i < 100000; ++i) {
+    winner = TieBreaker::resolve_tie(5);
+    ASSERT_LT(winner, 5);
+  }
 }
