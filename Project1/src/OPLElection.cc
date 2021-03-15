6e34ce1dbb25f423a324e30798c28fe751b137c6
Nikhil Srikanth <srika018@umn.edu>
2021-03-14 19:48:23 -0500

    Added audit logging and media report output to OPL Election

--- Project1/src/OPLElection.cc
+++ Project1/src/OPLElection.cc
@@ -13,6 +13,9 @@
 #include <iostream>
 
 OPLElection::OPLElection(std::string filename) : Election("OPL", filename) {
+  audit_log.log("Starting OPL Election");
+
+  audit_log.log("Parsing file header");
   std::string line, name, party;
   std::map<std::string, int> p_idx; // used to map the parties to their indexes in the vector
   int p_idx_count = 0; // map index counter
@@ -57,10 +60,22 @@ OPLElection::OPLElection(std::string filename) : Election("OPL", filename) {
       total_ballots = std::stoi(line);
     }
   }
+  audit_log.log("Finished parsing header");
+
+  // Log parsed header data
+  audit_log.log("Election Type: " + type);
+  audit_log.log("Total Ballots: " + std::to_string(total_ballots));
+
+  // Log parsed candidates
+  audit_log.log("Parsed " + std::to_string(candidates.size()) +
+                " Candididates:");
+  for (auto& can : candidates) {
+    audit_log.log(*can);
+  }
 }
 
 void OPLElection::parse_ballots() {
-  // TODO: finish parse ballots
+  audit_log.log("Parsing ballots");
 
   // File is already open from constructor of Election
   int candidate_idx = 0;
@@ -69,13 +84,20 @@ void OPLElection::parse_ballots() {
     OPLBallot temp(line);
     candidate_idx = temp.get_choice();
     if (candidate_idx != Ballot::NO_CHOICE) {
+      audit_log.log(temp);
       candidates.at(candidate_idx)->add_ballot(temp);
+    }
+    else {
+      // Discard ballot without choice
+      audit_log.log("Ballot with no choice discarded:");
+      audit_log.log(temp);
     }
   }
   // We can close ballot file here since we are probably done with it.
   // otherwise it will be closed in the destructor of Election
   ballot_file.close();
-  return;
+  
+  audit_log.log("Done Parsing Ballots");
 }
 
 void OPLElection::assign_seats() {
@@ -86,6 +108,7 @@ void OPLElection::assign_seats() {
 
   // determine quota
   int quota = (int)total_ballots / num_seats;
+  audit_log.log("The quota for this election is: " + std::to_string(quota));
 
   // assign initial seats to parties
   for(int i = 0; i < parties.size(); i++){
@@ -95,12 +118,15 @@ void OPLElection::assign_seats() {
     if(seats > parties.at(i).get_candidates().size()) {
       total_seats = total_seats - parties.at(i).get_candidates().size();
       party_seats[parties.at(i).get_name()] = parties.at(i).get_candidates().size();
+      audit_log.log(parties.at(i).get_name() + " won more seats than candidates so they are only given seats for the " + std::to_string(parties.at(i).get_candidates().size()) + " candidate(s) they have");
     }
     else {
       total_seats = total_seats - seats;
       party_seats[parties.at(i).get_name()] = seats;
+      audit_log.log(parties.at(i).get_name() + " won " + std::to_string(seats));
     }
     remainders.at(i) = parties.at(i).get_tally() % quota;
+    audit_log.log(parties.at(i).get_name() + " has a vote remainder of: " + std::to_string(remainders.at(i)));
 
     // get parties tallies
     party_votes.at(i) = parties.at(i).get_tally();
@@ -111,13 +137,16 @@ void OPLElection::assign_seats() {
     int index = std::distance(std::begin(remainders), most_rem);
     find_max_values(remainders, *most_rem);
     if(max_indicies.size() == 1) {
+      audit_log.log("Single party has largest remiander");
       party_seats[parties.at(index).get_name()]++;
       total_seats--;
       remainders.at(index) = 0;
     }
     else {
       // Tie break 
+      audit_log.log("Tie for largest remainder");
       int w_index = max_indicies.at(TieBreaker::resolve_tie(max_indicies.size()));
+      audit_log.log(parties.at(w_index).get_name() + " won the tie breaker");
       party_seats[parties.at(w_index).get_name()]++;
       total_seats--;
       remainders.at(w_index) = 0;
@@ -132,21 +161,26 @@ void OPLElection::assign_seats() {
 }
 
 void OPLElection::announce_results() {
-  // TODO: send information to media report
+  audit_log.log("ELECTION RESULTS: ");
   for(int i = 0; i < parties.size(); i++) {
+    std::stringstream output;
     std::vector<std::string> temp_candidates = party_candidates[parties.at(i).get_name()];
     int percent_of_total = parties.at(i).get_tally() * 100 / total_ballots;
     int percent_of_seats = party_seats[parties.at(i).get_name()] * 100 / num_seats;
-    std::cout << "Party " << parties.at(i).get_name() << " won " << party_seats[parties.at(i).get_name()] << " seats." << std::endl;
-    std::cout << "They won " << percent_of_total << "% of the total votes and " << percent_of_seats << "% of the seats" << std::endl;
-    std::cout << "The candidates that won those seats are: ";
+    output << "Party " << parties.at(i).get_name() << " won " << party_seats[parties.at(i).get_name()] << " seats." << std::endl;
+    output << "They won " << percent_of_total << "% of the total votes and " << percent_of_seats << "% of the seats" << std::endl;
+    output << "The candidates that won those seats are: ";
     for(int j = 0; j < temp_candidates.size(); j++) {
-      std::cout << temp_candidates.at(i) << " ";
+      output << temp_candidates.at(i) << " ";
     }
-    std::cout << std::endl << std::endl;
+    output << std::endl << std::endl;
+
+    audit_log.log(output.str());
+    std::cout << output.str();
+    media_report.write(output.str());
   }
 
-  return;
+  audit_log.log("END ELECTION RESULTS");
 }
 
 int OPLElection::run() {
