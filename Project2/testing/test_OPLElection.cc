7199f0527df9aeb56fe912c4a91cf9aec1284cca
Alex Bohm <bohm0080@umn.edu>
2021-04-27 22:03:13 -0500

    Added some testing for IR and OPL multiple files

--- Project2/testing/test_OPLElection.cc
+++ Project2/testing/test_OPLElection.cc
@@ -1,29 +1,44 @@
 // Copyright 2021, CSCI 5801 Spring 2021 Team 20
 #include "test_OPLElection.h"
+
 #include <unistd.h>
+
+#include <string>
+#include <vector>
+
 #include "OPLBallot.h"
 #include "OPLCandidate.h"
 #include "OPLParty.h"
+
 unsigned int microsecond = 1000000;
 TEST_F(fixture_OPLElection, OPLElectionFunctionality) {
   // Test basic functionality
   election = new OPLElection("./ballot_files/opl_random.csv");
-  ASSERT_EQ(election->run(),0);
+  ASSERT_EQ(election->run(), 0);
 }
-// TEST_F(fixture_OPLElection, OPLElectionMorePartiesThanSeats) {
-//   election = new OPLElection("./ballot_files/opl_example.csv");
-//   ASSERT_EQ(election->run(),0);
-// }
+TEST_F(fixture_OPLElection, opl_example) {
+  OPLElection test("ballot_files/opl_example.csv");
+
+  ASSERT_EQ(test.run(), 0);
+}
+TEST_F(fixture_OPLElection, multiple_files) {
+  OPLElection test(std::vector<std::string>{
+      "ballot_files/opl_half_first.csv", "ballot_files/opl_half_second.csv"});
+
+  ASSERT_EQ(test.run(), 0);
+}
 
 TEST_F(fixture_OPLElection, OPLElection3waytie) {
-  // Test whether each of the 3 parties wins the tie an approximately equal number of times
+  // Test whether each of the 3 parties wins the tie an approximately equal
+  // number of times
   election = new OPLElection("./ballot_files/opl_partyTie.csv");
   int dwins = 0;
   int rwins = 0;
   int iwins = 0;
-   for(int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     std::cout << "in for loop, iteration #" << i << "\n";
-    // Redirect cout stream to a buffer, collecting output which lists who won how many seats
+    // Redirect cout stream to a buffer, collecting output which lists who won
+    // how many seats
     std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
     std::ostringstream strCout;
     std::cout.rdbuf(strCout.rdbuf());
@@ -34,22 +49,22 @@ TEST_F(fixture_OPLElection, OPLElection3waytie) {
     // Read out printout line by line
     std::istringstream f(strCout.str());
     std::string line;
-    while(std::getline(f,line)) {
+    while (std::getline(f, line)) {
       std::cout << line << "\n";
-      if(line.compare("Party D won 1 seats.") == 0) {
-         dwins++;
-       }
-      if(line.compare("Party R won 1 seats.") == 0) {
-         rwins++;
-       }
-      if(line.compare("Party I won 1 seats.") == 0) {
+      if (line.compare("Party D won 1 seats.") == 0) {
+        dwins++;
+      }
+      if (line.compare("Party R won 1 seats.") == 0) {
+        rwins++;
+      }
+      if (line.compare("Party I won 1 seats.") == 0) {
         iwins++;
       }
     }
-   }
+  }
   std::cout << "D won " << dwins << " times\n";
   std::cout << "R won " << rwins << " times\n";
   std::cout << "I won " << iwins << " times\n";
-  float prop = round(100*(float(dwins) / float(1000000)));
+  float prop = round(100 * (float(dwins) / float(1000000)));
   ASSERT_EQ(prop, 33);
 }
