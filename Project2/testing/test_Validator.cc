3b1c9195eb5cce9cfb939014d4ca61f67108e47f
linde764 <linde764@csel-kh1250-05.cselabs.umn.edu>
2021-04-28 23:38:12 -0500

    Fixing validation tests

--- Project2/testing/test_Validator.cc
+++ Project2/testing/test_Validator.cc
@@ -7,17 +7,20 @@
 TEST_F(fixture_VALIDATOR, validballot) {
     IRBallot ballot("1,2,3,4");
     bool valid = Validator::validate(ballot.get_choices().size(),4);
-    ASSERT_LT(valid, 1);
+    bool result = true;
+    ASSERT_EQ(valid, result);
 }
 
 TEST_F(fixture_VALIDATOR, validballot_morethanhalf) {
     IRBallot ballot("1,2,3,,");
     bool valid = Validator::validate(ballot.get_choices().size(),4);
-    ASSERT_LT(valid, 1);
+    bool result = true;
+    ASSERT_EQ(valid, result);
 }
 
 TEST_F(fixture_VALIDATOR, invalidballot) {
     IRBallot ballot(",,,,");
     bool valid = Validator::validate(ballot.get_choices().size(),4);
-    ASSERT_LT(valid, 0);
+    bool result = false;
+    ASSERT_EQ(valid, result);
 }
